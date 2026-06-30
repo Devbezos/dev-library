c5d36e3ad832a0cb69e41ff5a73ee63d6017af47
Devbezos <devbezos1994@gmail.com>
2026-06-29 23:24:26 -0400

    Remove droptimizer sync job constant

--- Data/Constants.cs
+++ Data/Constants.cs
@@ -8,7 +8,6 @@ namespace DevClient.Data
             public const string FitnessWeekly           = "fitness_weekly";
             public const string DroptimizerReminder     = "droptimizer_reminder";
             public const string RaidReminder            = "raid_reminder";
-            public const string DroptimizerSync         = "droptimizer_sync";
             public const string ServerAvailability      = "server_availability";
             public const string KeyAudit                = "key_audit";
             public const string Tcg                     = "tcg";
